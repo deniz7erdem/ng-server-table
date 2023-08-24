1412847e62c32050eb9bdd7e38710ed06f8d097a
Deniz Erdem <derdemm7@gmail.com>
2023-08-24 23:30:31 +0300

    ready for simple tests

--- api/app.js
+++ api/app.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const cors = require('cors');
+const pg = require('./db');
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.post('/api/users', async (req, res) => {
+    try {
+        const { pageNo, perPage } = req.body;
+        const offset = (pageNo - 1) * perPage;
+        console.warn(offset);
+        const users = await pg.query('SELECT * FROM users ORDER BY id ASC LIMIT $1 OFFSET $2', [perPage, offset]);
+        res.json(users.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log('server is live on port 3000')
+    console.log('server is live on port 3000');
+    pg.connect(err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Postgresql'e başarıyla bağlanıldı.");
+        }
+    });
 });
